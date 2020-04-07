117fc1224c9968a0eedb1f858a33f0f90eb2b5cb
ballz <ballz@ubuntu.vm>
2020-04-07 11:00:19 +0100

    Changing mk's

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,9 +18,14 @@ LOCAL_PATH := device/archos/Alba10
 
 TARGET_BOARD_PLATFORM := mt8163
 TARGET_BOOTLOADER_BOARD_NAME := Alba10
+TARGET_NO_BOOTLOADER := true
 
 # Recovery
+BOARD_USES_MTK_HARDWARE := true
+BOARD_HAS_MTK_HARDWARE := true
+
 TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 8216576
 BOARD_FLASH_BLOCK_SIZE := 0
 BOARD_HAS_NO_REAL_SDCARD := true
@@ -29,7 +34,34 @@ BOARD_HAS_NO_MISC_PARTITION := true
 BOARD_RECOVERY_SWIPE := true
 BOARD_USES_MMCUTILS := true
 BOARD_SUPPRESS_EMMC_WIPE := true
+BOARD_CHARGER_SHOW_PERCENTAGE := true
+
+#TWRP Stuff
+
+TW_EXCLUDE_SUPERSU := true                     # true/false: Add SuperSU or not
+TW_INCLUDE_CRYPTO := true                      # true/false: Add Data Encryption Support or not
 TW_INPUT_BLACKLIST := "hbtp_vm"
+TW_SCREEN_BLANK_ON_BOOT := true
+DEVICE_RESOLUTION := 720x1280                  # The Resolution of your Device
+TARGET_SCREEN_HEIGHT := 1280                     # The height
+TARGET_SCREEN_WIDTH := 720                       # The width
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBA_8888"
+# Set the Brightness Control File Path below
+TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
+TW_SECONDARY_BRIGHTNESS_PATH := /sys/devices/platform/leds-mt65xx/leds/lcd-backlight/brightness
+# Set the Path of Logical Units (LUNs) for Storage below
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/mt_usb/musb-hdrc.0.auto/gadget/lun%d/file
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storage/lun/file
+TW_MAX_BRIGHTNESS := 255
+TW_DEFAULT_BRIGHTNESS := 80                   # Set custom brightness, low is better
+
+TW_INCLUDE_NTFS_3G := true                    # Include NTFS Filesystem Support
+TW_INCLUDE_FUSE_EXFAT := true                 # Include Fuse-ExFAT Filesystem Support
+TWRP_INCLUDE_LOGCAT := true                   # Include LogCat Binary
+TW_INCLUDE_FB2PNG := true                     # Include Screenshot Support
+TW_DEFAULT_LANGUAGE := en                     # Set Default Language 
+TW_EXTRA_LANGUAGES := false
+
 include $(LOCAL_PATH)/kernel.mk
 include device/generic/twrpbuilder/BoardConfig64.mk
 
--- kernel.mk
+++ kernel.mk
@@ -20,3 +20,13 @@ BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x40078000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x03f88000 --tags_offset 0x0df88000
+
+TARGET_BOARD_SUFFIX := _64                    # Remove if the device is 32-bit
+TARGET_USES_64_BIT_BINDER := true             # Remove if the device is 32-bit
+
+# Check whether the device is 64-bit or 32-bit, and then include the TWRPBuilder Part
+ifeq ($(TARGET_IS_64_BIT),true)
+include device/generic/twrpbuilder/BoardConfig64.mk   # If the device Is 64-bit, it'll be used
+else
+include device/generic/twrpbuilder/BoardConfig32.mk   # If the device Is 32-bit, it'll be used
+endif
